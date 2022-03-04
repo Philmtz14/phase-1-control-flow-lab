a1285b251f01e7442ac01fdd79e62b45b85cdae3
philip martinez <martinez.philip00@gmail.com>
2022-03-04 04:52:00 -0600

    commit tree fd94b5c4d2609248d277b1affdd8f048c07460bb

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue) {
   // Write your code here!
+  let result
+  if (someValue <= 199) {
+    result = 'gives customers a free sample if the ride is less than or equal to 400 feet';
+    return 'This one is on me!'
+  } else if (someValue <= 2500) {
+    result = 'charges 30 dollars for a distance over 2000 feet';
+    return 'I will gladly take your thirty bucks.';
+  } else (someValue > 2500); {
+    result = 'does not allow rides over 2500 feet';
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city) {
+      return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+
+function switchOnCharmFromTip(attitude) {
+    switch (attitude) {
+      case attitude = 'generous':
+        return 'Thank you so much.';
+      case attitude = 'not as generous':
+        return 'Thank you.';
+      case attitude = 'thanks for everything':
+        return 'Bye.';
+    }
+  }
